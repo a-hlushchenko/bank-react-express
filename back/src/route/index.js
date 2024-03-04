1d5d4ffb1c53ccc12f6955ea6cb123e90737f55f
Anton <glantoshka@gmail.com>
2024-03-04 23:59:05 +0200

    create

---
+++ back/src/route/index.js
@@ -0,0 +1,20 @@
+// Підключаємо роутер до бек-енду
+const express = require('express')
+const router = express.Router()
+
+// Підключіть файли роутів
+const auth = require('./auth')
+const dashboard = require('./dashboard')
+// Підключіть інші файли роутів, якщо є
+
+// Об'єднайте файли роутів за потреби
+router.use('/', auth)
+router.use('/', dashboard)
+// Використовуйте інші файли роутів, якщо є
+
+router.get('/', (req, res) => {
+  res.status(200).json('Hello World')
+})
+
+// Експортуємо глобальний роутер
+module.exports = router
